a8a03b38777e6e8b410c0b80e298fe4f469e9f40
Spirit Rosenberg <230562491+rosenbergspirit-debug@users.noreply.github.com>
2026-04-01 13:40:17 +0200

    Added a matplotlib and histogram

--- backend/routes/analysis.py
+++ backend/routes/analysis.py
@@ -5,9 +5,14 @@
 # import pandas as pd importerar pandas biblioteket och ger det förkortning pd. Ex. pd.read_csv() istället för pandas.read_csv().
 # import io är ett inbyggt python bibliotek för att hantera data i minnet som om det va en fil. När FastAPI tar emot en uppladdad fil är innehållet binärt i 1or och 0or.
 # io.BytesIO() omvandlar dessa 1or och 0or (bytes) till fil liknande objekt som pandas kan läsa direkt utan något behöver sparas på disken 
+# import Matplotlib och gör så att den kan köras utan skärm för att ej krascha
 from fastapi import APIRouter, UploadFile, File, HTTPException
 import pandas as pd
 import io
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from fastapi.responses import StreamingResponse
 
 # Detta skapar routern med prefix /analysis där alla endpoints får /analysis framför sig
 router = APIRouter(prefix="/analysis", tags=["analysis"])
@@ -79,4 +84,44 @@ async def analysera_csv(fil: UploadFile = File(...)):
         }
     }
 
+# Tar emot csv och returnerar histogram som bild istället för json via straming response
+@router.post("/histogram")
+async def rita_histogram(fil: UploadFile = File(...)):
 
+    # Anger samma villkor för korrekt inläsning
+    if not fil.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="Endast CSV-filer stöds")
+    innehall = await fil.read()
+    df = pd.read_csv(io.BytesIO(innehall))
+    numeriska = df.select_dtypes(include="number")
+
+    if numeriska.empty:
+        raise HTTPException(status_code=400, detail="Inga numeriska kolumner")
+    
+    # Räknar numeriska kolumner
+    antal_kolumner = len(numeriska.columns)
+    fig, axar = plt.subplots(1, antal_kolumner, figsize=(5 * antal_kolumner, 4))
+
+    # Om det bara finns en kolumn blir axar ej en lista
+    if antal_kolumner == 1:
+        axar = [axar]
+
+    # Loopar igenom alla numeriska kolumner och ritar ut ett histogram
+    for ax, kolumn in zip(axar, numeriska.columns):
+        # inställningar
+        numeriska[kolumn].hist(ax=ax, bins=20, color="#5DCAA5", edgecolor="white")
+        ax.set_title(kolumn)    
+        ax.set_xlabel("Värde")
+        ax.set_ylabel("Antal")
+
+    # autojustering av spacing vid diagram
+    plt.tight_layout()
+
+    # io.BytesIO() skapar minnesobjekt som blir som en fil där bild kan sparas
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png", dpi=120)
+    buf.seek(0)
+    plt.close()
+
+    # Streaming response skickar bilden till frontend
+    return StreamingResponse(buf, media_type="image/png")
